9ca7699bab9d8cb320fb7b8c67d865fa7e938b45
Mike Stall <mikestall@hotmail.com>
2016-08-03 22:52:47 -0700

    Search working. Added HTML helper functions.

--- src/pluginmain.ts
+++ src/pluginmain.ts
@@ -3,11 +3,14 @@
 // This calls TRC APIs and binds to specific HTML elements from the page.  
 
 import * as trc from '../node_modules/trclib/trc2';
+import * as html from './trchtml';
 
-declare var $ :any; // external definition for JQuery 
+declare var $: any; // external definition for JQuery 
 
-export class MyPlugin {   
+export class MyPlugin {
     private _sheet: trc.Sheet;
+    private _data: trc.ISheetContents;
+    private _info: trc.ISheetInfoResult;
 
     // Entry point called from brower. 
     public static BrowserEntry(sheet: trc.ISheetReference): MyPlugin {
@@ -16,41 +19,154 @@ export class MyPlugin {
     }
 
     public constructor(sheet: trc.Sheet) {
-        this.resetUi();
-        this._sheet = sheet; // Save for when we do Post               
+        this._sheet = sheet; // Save for when we do Post
 
-        this.refresh();
+/*
+        var result : trc.ISheetContents =  { };
+        result["Alpha"] = ["A1", "A2"];
+        result["Beta"] = ["B1", "B2"];
+        result["Charlie"] = ["C1", "C2"];
+
+        var render = new html.RenderSheetOptions(result);
+        render.setColumns(["Alpha", "Charlie"]);
+
+        render.setHtml("Alpha", (iRow : number) => {
+            var recId = result["Alpha"][iRow];
+            return "<b>" + recId+ "</b>";
+        });
+        render.renderSheet("main");
+*/
     }
 
-    private refresh() {
-        this._sheet.getInfo(
-            (info) => {
-                this.updateInfo(info);
+    public initAsync(next: () => void): void {
+        this._sheet.getInfo(info => {
+            this._info = info;
+            this._sheet.getSheetContents((data) => {
+                this._data = data;
+                this.resetUi();
+                next();
             });
+        });
     }
+
+    
 
     protected resetUi(): void {
         // clear previous results
         $('#main').empty();
+        $("#startMsg").text(this._info.CountRecords + " total records.");
+    }  
+  
+    // Button click handler
+    public onSearch(): void {
+        // Get criteria from UX
+        var first = $("#First").val();
+        var last = $("#Last").val();
+        var city = $("#City").val();
+        var zip = $("#Zip").val();
+
+        // In-memory search 
+        var result = this.search2(first, last, city, zip);
+
+        // Render to sheet. 
+        //MyPlugin.renderSheetToDiv(result, "main");
+        var render = new html.RenderSheetOptions(result);
+        render.setColumnInfo(this._info.Columns);
+        // render.setColumns(["RecId", "FirstName", "LastName"])
+        render.setHtml("RecId", (iRow : number) => {
+            var recId = result["RecId"][iRow];
+            return "jump to <b>" + recId+ "</b>";
+        });
+        render.renderSheet("main");
+
+        $("#countMsg").html("Found <b>" + render.getCountRows() + "</b> row(s) matching criteria.");
     }
 
-    // Display sheet info on HTML page
-    public updateInfo(info: trc.ISheetInfoResult): void {
-        $("#SheetName").text(info.Name);
-        $("#ParentSheetName").text(info.ParentName);
-        $("#SheetVer").text(info.LatestVersion);
-        $("#RowCount").text(info.CountRecords);
-
-        $("#LastRefreshed").text(new Date().toLocaleString());
+    private static norm(x: string): string {
+        if (x == null || x == undefined || x.length == 0) {
+            return null;
+        }
+        return x.toUpperCase();
     }
 
-    // Example of a helper function.
-    public doubleit(val: number): number {
-        return val * 2;
+    // Put this in a common helper library $$$  
+    // applies fpInclude on each row in source sheet. 
+    // Returns a new sheet with same columns, but is a subset.  
+    public static GetRows(
+        source: trc.ISheetContents,
+        fpInclude: (idx: number) => boolean)
+        : trc.ISheetContents {
+        var columnNames: string[] = [];
+        var results: trc.ISheetContents = {};
+        for (var columnName in source) {
+            columnNames.push(columnName);
+            results[columnName] = [];
+        }
+
+        var cRecId: string[] = source["RecId"];
+        //for(var iRow  in cRecId)
+        for (var iRow = 0; iRow < cRecId.length; iRow++) {
+            var keepRow: boolean = fpInclude(iRow);
+            if (keepRow) {
+                for (var x in columnNames) {
+                    var columnName = columnNames[x];
+                    var val = source[columnName][iRow];
+                    results[columnName].push(val)
+                }
+            }
+        }
+        return results;
     }
 
-    // Demonstrate receiving UI handlers 
-    public onClickRefresh(): void {
-        this.refresh();
+    // target is already normalized. other is not. 
+    private static Mismatch(target : string, other: string) : boolean
+    {
+        if (target == null) {
+            return false;
+        }
+        other = MyPlugin.norm(other); 
+        return (other != target);
+    }
+
+    // Return the rows in _data that match the filter.  
+    public search2(
+        first: string,
+        last: string,
+        city: string,
+        zip: string
+    ): trc.ISheetContents {
+        first = MyPlugin.norm(first);
+        last = MyPlugin.norm(last);
+        city = MyPlugin.norm(city);
+        zip = MyPlugin.norm(zip);
+
+        var cFirst = this._data["FirstName"];
+        var cLast = this._data["LastName"];
+        var cCity = this._data["City"];
+        var cZip = this._data["Zip"];
+
+        // $$$ Compare - if ends in "*", do a suffix match. 
+        var result = MyPlugin.GetRows(this._data,
+            (iRow) => {
+                if (MyPlugin.Mismatch(last, cLast[iRow]))                
+                {
+                    return false;   
+                }
+                if (MyPlugin.Mismatch(first, cFirst[iRow]))
+                {
+                    return false;   
+                }
+                if (MyPlugin.Mismatch(city, cCity[iRow]))
+                {
+                    return false;   
+                }
+                if (MyPlugin.Mismatch(zip, cZip[iRow]))
+                {
+                    return false;   
+                }
+                return true;
+            });
+
+        return result;
     }
 }
